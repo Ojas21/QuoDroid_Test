10fcafc809600439b34ed7e6e23b97b794f72446
Ojas21 <65960254+Ojas21@users.noreply.github.com>
2024-04-11 15:12:56 +0530

    improved readibility

--- testai_project/testai_app/views.py
+++ testai_project/testai_app/views.py
@@ -18,34 +18,34 @@ Library    SeleniumLibrary
 """
             for test in tests:
                 steps = test.get('steps', [])
-                # Adjust the steps to match the correct syntax
+    
                 adjusted_steps = []
+                #extracting .robot keywords
                 for step in steps:
                     if "Open Browser" in step:
-                        # Adjust the Open Browser step
+                     
                         browser = step.split("browser='")[1].split("'")[0]
                         
                     elif "Go To" in step:
-                        # Adjust the Go To step
+               
                         url = step.split("url='")[1].split("'")[0]
                         url="".join(url)
                         
                     else:
                         adjusted_steps.append(step)
-                # Assuming each test in the payload has a unique name or identifier
-                # Here, we're using a simple counter as an identifier for demonstration
+                #creating the robot file
                 adjusted_steps.append(f"Open Browser  {url}  {browser}\n")
 
                 test_suite += f"""Test {test['title']}
     {'    '.join(adjusted_steps)}
 """
                 
-            # Save the test suite to a file
+          
             with open('test_suite.robot', 'w') as f:
                 f.write(test_suite)
-            # Execute the test suite using Robot Framework
+            # Executing the test suite using Robot Framework
             result = subprocess.run(['robot', 'test_suite.robot'], capture_output=True, text=True)
-            # Return the result
+            # Returning the results
            
             return JsonResponse({'result': result.stdout})
         except Exception as e:
